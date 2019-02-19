9cf335a0ff81f7f1021ccf792733f59ca58c97b7
Evan Straw <evan.straw99@gmail.com>
2019-02-18 21:49:31 -0800

    Add a lot of language-specific configuration
    Added a lot of stuff for LSP java config, as well as some stuff for
    C/C++ development. Will probably be replacing cquery with ccls
    sometime in the future.

--- custom.el
+++ custom.el
@@ -9,8 +9,8 @@
  '(magit-commit-arguments (quote ("-S")))
  '(package-selected-packages
    (quote
-    (smartparens geiser yasnippet-snippets yasnippet projectile use-package treemacs lsp-java flycheck lsp-ui company-lsp company lsp-mode magit doom-themes doom-modeline))))
-(custom-set-faces
+    (cquery column-enforce-mode rainbow-delimiters pdf-tools gradle-mode smartparens geiser yasnippet-snippets yasnippet projectile use-package treemacs lsp-java flycheck lsp-ui company-lsp company lsp-mode magit doom-themes doom-modeline))))
+ (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;; ----------------
 (require 'package)
 ;; Add MELPA
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; ---------------
@@ -20,6 +20,11 @@
 ;; ---------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; ---------------
 ;; Install selected packages that aren't installed
@@ -48,12 +53,13 @@
   "`doom-themes-configuration' is interactive by the modeline, it will
 start the nice defaults for `doom-one' and establish `doom-modeline-mode'."
   (interactive)
-  (load-theme 'doom-one t)
+  (load-theme 'doom-molokai t)
   (setq doom-themes-enable-bold t      ; If nil, bold is universally disabled
 	doom-themes-enable-italic t)   ; If nil, italics is universally disabled
   (doom-themes-visual-bell-config)
   (doom-themes-org-config)
-  (doom-modeline-mode))
+  (doom-modeline-mode)
+  (setq doom-modeline-buffer-file-name-style 'file-name))
 
 (doom-themes-configuration) ; Execute `doom-themes-configuration'
 
@@ -61,10 +67,51 @@ start the nice defaults for `doom-one' and establish `doom-modeline-mode'."
 ;; Generic configuration for all languages
 ;; ---------------
 (require 'smartparens-config)
+(use-package column-enforce-mode :ensure t)
 (smartparens-global-mode)
+(global-hl-line-mode)
+(column-number-mode)
+
+;; turn on warning at 95 columns
+(setq column-enforce-column 95)
+
+;; make company dialog show up sooner
+(setq company-idle-delay 0.1)
+
 (add-hook 'prog-mode-hook
 	  '(lambda ()
-	     (company-mode)))
+	     (company-mode)
+	     (linum-mode)
+	     (rainbow-delimiters-mode)
+	     (column-enforce-mode)
+	     (gradle-mode)))
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+;; ---------------
+;; Smartparens config for CC-like modes
+;; ---------------
+(sp-with-modes '(java-mode c++-mode)
+  (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
+  (sp-local-pair "/**" "*/" :post-handlers '(("| " "SPC")
+     					     (" ||\n[i]" "RET"))))
+;;  (sp-local-pair "/*" nil)
+;;  (sp-local-pair "/*" "*/" :post-handlers '((" | " "SPC")
+
+;;  (sp-local-pair "/**" "*/" :post-handlers '((" | " "SPC")
+;;					     (" ||\n[i]" "RET"))))
+
+
+(global-set-key "\C-c\C-n" 'flycheck-mode)
+
+;; ---------------
+;; PDF-Tools config
+;; ---------------
+(use-package pdf-tools :ensure t)
+(pdf-tools-install)
 
 ;; ---------------
 ;; Generic LSP Configuration
@@ -77,14 +124,23 @@ start the nice defaults for `doom-one' and establish `doom-modeline-mode'."
 (use-package company-lsp :ensure t)
 (use-package lsp-ui :ensure t)
 
+(add-hook 'c-mode-common-hook
+		  '(lambda ()
+			 (setq tab-width 4)))
+
 ;; ---------------
 ;; LSP Java Configuration
 ;; ---------------
 (use-package lsp-java :ensure t :after lsp)
+(use-package gradle-mode :ensure t)
+(setq lsp-java-import-gradle-enabled nil)
 (add-hook 'java-mode-hook
-		    '(lambda () 
+		    '(lambda ()
 		      (lsp)
-		      (flycheck-mode)))
+		      (flycheck-mode)
+		      (local-set-key "\C-c\C-g" 'gradle-run)
+		      (local-set-key "\C-c\C-r" 'lsp-rename)
+		      (local-set-key "\C-c\C-f" 'lsp-format-buffer)))
 ;;(add-hook 'java-mode-hook 'flycheck-mode)
 
 ;;(use-package dap-mode
@@ -95,3 +151,25 @@ start the nice defaults for `doom-one' and establish `doom-modeline-mode'."
 
 ;;(use-package dap-java :after (lsp-java))
 (use-package lsp-java-treemacs :after (treemacs))
+
+;; ---------------
+;; Cquery configuration (C/C++)
+;; ---------------
+(require 'cquery)
+(setq cquery-executable "/usr/bin/cquery")
+(setq cquery-extra-init-params '(:index (:comments 2) :cacheFormat "msgpack"))
+(defun cquery//enable ()
+  (condition-case nil
+      (lsp)
+    (user-error nil)))
+(add-hook 'c-mode-hook #'cquery//enable)
+(add-hook 'c++-mode-hook #'cquery//enable)
+
+;; ---------------
+;; Configure pdf-tools
+;; ---------------
+(use-package pdf-tools :ensure t)
+(add-hook 'pdf-view-mode-hook
+	  '(lambda ()
+	     (setq pdf-view-midnight-colors '("white" . "#1c1e1f"))
+	     (pdf-view-midnight-minor-mode)))
