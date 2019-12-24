5bfc2bb866c0e7cb22067999d383fa3213ba92cf
Evan Straw <evan.straw99@gmail.com>
2019-12-23 18:42:32 -0800

    Overhaul web languages config to use use-package

--- modules/module-lang-web.el
+++ modules/module-lang-web.el
@@ -1,59 +1,31 @@
 (use-package web-beautify
-  :ensure t
-  :defer t)
-(use-package impatient-mode
-  :ensure t
-  :defer t)
+  :commands (web-beautify-html
+	     web-beautify-css
+	     web-beautify-js))
+
 (use-package php-mode
-  :ensure t
-  :defer t)
-;;(use-package flymd
-;;  :ensure t)
+  :hook (php-mode . (lambda () (c-set-style "wordpress"))))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . js-mode))
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :bind (("C-c b" . web-beautify-js)
+	 ("C-c C-r" . lsp-rename))
+  :hook (js2-mode . lsp))
 
-(eval-after-load 'js2-mode (lambda ()
-  (define-key js2-mode-map (kbd "C-c b") 'web-beautify-js)
-  (define-key js2-mode-map (kbd "C-c C-r") 'lsp-rename)))
-;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-(eval-after-load 'js
-  '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
+(use-package json-mode
+  :bind ("C-c b" . web-beautify-js))
 
-(eval-after-load 'json-mode
-  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+(use-package sgml-mode
+  :bind (("C-c b" . web-beautify-html)
+	 ("C-c C-f" . sp-html-next-tag)
+	 ("C-c C-b" . sp-html-previous-tag))
+  :hook (html-mode . lsp))
 
-(eval-after-load 'sgml-mode (lambda ()
-  (define-key html-mode-map (kbd "C-c b") 'web-beautify-html)
-  (define-key html-mode-map (kbd "C-c C-f") 'sp-html-next-tag)
-  (define-key html-mode-map (kbd "C-c C-b") 'sp-html-previous-tag)))
+(use-package css-mode
+  :bind ("C-c b" . web-beautify-css)
+  :hook (css-mode . lsp))
 
-(eval-after-load 'web-mode
-  '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
-
-(eval-after-load 'css-mode
-  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
-
-(add-hook 'html-mode-hook
-	  (lambda ()
-	    (lsp)
-	    (httpd-start)
-	    (impatient-mode)))
-
-(add-hook 'css-mode-hook
-	  (lambda ()
-	    (lsp)))
-
-(add-hook 'js2-mode-hook
-	  (lambda ()
-	    (lsp)))
-
-(add-hook 'php-mode-hook
-	  (lambda ()
-	    (c-set-style "wordpress")))
-
-(add-hook 'sql-interactive-mode-hook
-          (lambda ()
-            (toggle-truncate-lines t)))
+(use-package sql
+  :hook (sql-interactive-mode . (lambda () (toggle-truncate-lines t))))
 
 (provide 'module-lang-web)
