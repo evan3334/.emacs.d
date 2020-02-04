76d24a5ad8763762008151563bcd0666d1854c31
Evan Straw <evan.straw99@gmail.com>
2020-02-03 16:57:08 -0800

    Revert Gnus config from use-package

--- modules/module-email.el
+++ modules/module-email.el
@@ -2,24 +2,25 @@
   :commands (gitpatch-mail
 	     gitpatch-mail-attach-patch))
 
-(use-package gnus
-  :commands (gnus)
-  :config
-  (setq gnus-select-method '(nnnil ""))
-  (setq gnus-secondary-select-methods '((nnml "")
-					(nnimap "imap.gmail.com")))
-  
-  (setq user-full-name "Evan Straw"
-	user-mail-address "evan.straw99@gmail.com")
-  
-  (setq message-send-mail-function 'smtpmail-send-it
-	smtpmail-default-smtp-server "smtp.gmail.com")
-  
-  (setq gnus-mime-display-multipart-related-as-mixed t)
-  (setq gnus-auto-select-first nil)
-  (setq gnus-summary-display-arrow nil)
-  (setq gnus-buttonized-mime-types '("multipart/signed"))
-  :hook ((gnus-group-mode . gnus-topic-mode)
-	 (gnus-article-mode . emojify-mode)))
+(require 'gnus)
+(setq gnus-select-method '(nnnil ""))
+(setq gnus-secondary-select-methods '((nnml "")
+				      (nnimap "imap.gmail.com")))
+
+(setq user-full-name "Evan Straw"
+      user-mail-address "evan.straw99@gmail.com")
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-default-smtp-server "smtp.gmail.com")
+
+(setq gnus-mime-display-multipart-related-as-mixed t)
+(setq gnus-auto-select-first nil)
+(setq gnus-summary-display-arrow nil)
+
+(setq gnus-buttonized-mime-types '("multipart/signed"))
+(setq mm-verify-option 'always)
+
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+(add-hook 'gnus-article-mode-hook 'emojify-mode)
 
 (provide 'module-email)
