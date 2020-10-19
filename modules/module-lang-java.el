9221c61ff75a0ed80c5c04b264c20a1d61bbe571
Evan Straw <evan.straw99@gmail.com>
2020-10-18 17:22:14 -0700

    Greatly improve java config

--- modules/module-lang-java.el
+++ modules/module-lang-java.el
@@ -1,33 +1,35 @@
-(add-hook 'c-mode-common-hook
-		  '(lambda ()
-			 (setq tab-width 4)))
-
-(global-set-key "\C-c\C-n" 'flycheck-mode)
-
 ;; ---------------
 ;; LSP Java Configuration
 ;; ---------------
-(use-package lsp-java :ensure t :after lsp)
-(use-package gradle-mode :ensure t)
-(setq lsp-java-import-gradle-enabled nil)
-(add-hook 'java-mode-hook
-		    '(lambda ()
-		      (lsp)
-		      (flycheck-mode)
-		      (local-set-key "\C-c\C-g" 'gradle-run)
-		      (local-set-key "\C-c\C-r" 'lsp-rename)
-		      (local-set-key (kbd "C-c RET") 'lsp-execute-code-action)
-		      (local-set-key "\C-c\C-f" 'lsp-format-buffer)))
-;;(add-hook 'java-mode-hook 'flycheck-mode)
+(use-package lsp-java :ensure t :after lsp-mode
+  :commands (lsp-java-lens-mode)
+  :config
+  (setq lsp-java-server-install-dir
+	(expand-file-name "eclipse.jdt.ls" user-emacs-directory))
+  (setq lsp-java-import-gradle-enabled nil)
+  (setq tab-width 4)
+  (use-package lsp-treemacs)
+  :bind (:map java-mode-map
+	      ("C-c C-g" . gradle-run)
+	      ("C-c C-r" . lsp-rename)
+	      ("C-c RET" . lsp-execute-code-action)
+	      ("C-c C-f" . lsp-format-buffer)))
 
-;;(use-package dap-mode
-;;  :ensure t :after lsp-mode
-;;  :config
-;;  (dap-mode t)
-;;  (dap-ui-mode t))
+(use-package lsp-mode
+  :commands (lsp)
+  :hook ((java-mode . lsp)
+		 (java-mode . lsp-java-lens-mode)))
 
-;;(use-package dap-java :after (lsp-java))
-;;(use-package lsp-java-treemacs :after (treemacs))
+(use-package gradle-mode :ensure t
+  :commands (gradle-run))
 
+(use-package flycheck
+  :commands (flycheck-mode)
+  :hook (java-mode . flycheck-mode))
+
+(use-package dap-mode :ensure t :after lsp-mode
+  :commands (dap-mode)
+  :hook (java-mode . dap-mode)
+  :config (dap-auto-configure-mode))
 
 (provide 'module-lang-java)
