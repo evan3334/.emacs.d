c1c89017e99e33d59ddbb1bb5cb70bbff8b8c29b
Evan Straw <evan.straw99@gmail.com>
2020-12-06 18:24:27 -0800

    Add function to colorize the compilation buffer for JUnit tests

--- modules/module-lang-general.el
+++ modules/module-lang-general.el
@@ -95,4 +95,11 @@
 (use-package realgud
   :commands (realgud:gdb))
 
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 (provide 'module-lang-general)
