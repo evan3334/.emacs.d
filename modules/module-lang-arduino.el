0fe5d7446e45d9102b2d733088cee61a933374d9
Evan Straw <evan.straw99@gmail.com>
2022-03-14 18:48:17 -0700

    Add Arduino LSP server

--- modules/module-lang-arduino.el
+++ modules/module-lang-arduino.el
@@ -2,6 +2,46 @@
   :defer t
   :hook (arduino-mode . (lambda ()
 			  (flycheck-mode)
-			  (rainbow-delimiters-mode))))
+			  (rainbow-delimiters-mode)
+			  (lsp))))
+
+(defgroup lsp-arduino nil
+  "LSP support for Arduino."
+  :group 'lsp-mode)
+
+(defcustom lsp-arduino-executable "arduino-language-server"
+  "Path of the Arduino LSP executable."
+  :group 'lsp-arduino
+  :type 'string)
+
+(defcustom lsp-arduino-compile-commands-dir nil
+  "Path of the compile_commands.json file."
+  :group 'lsp-arduino
+  :type 'string)
+
+(defcustom lsp-arduino-cli-config-path "~/.arduino15"
+  "Path where the arduino-cli configuration file can be found."
+  :group 'lsp-arduino
+  :type 'string)
+
+(defcustom lsp-arduino-fbqn nil
+  "Fully Qualified Board Name (FBQN) that should be used."
+  :group 'lsp-arduino
+  :type 'string)
+
+(defun arduino-lsp--server-command ()
+  "Startup command for Arduino language server."
+  (list lsp-arduino-executable
+		(format "-cli-config=%s" lsp-arduino-cli-config-path)
+		"-log"
+		(when lsp-arduino-compile-commands-dir (format "-compile-commands-dir=%s" lsp-arduino-compile-commands-dir))
+		(when lsp-arduino-fbqn (format "-fbqn=%s" lsp-arduino-fbqn))))
+
+(use-package lsp-mode
+  :config
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection #'arduino-lsp--server-command)
+					:major-modes '(arduino-mode)
+					:server-id 'arduino)))
 
 (provide 'module-lang-arduino)
