17a71ff6793422e79b7aeab7ea3729c1357e0f84
Evan Straw <evan.straw99@gmail.com>
2022-05-18 02:52:31 -0700

    Switch to clangd instead of CCLS
    Squashed commit of the following:
    
    commit 0ee04d639aacc17bae08d91c90bbe42c8ace7493
    Author: Evan Straw <evan.straw99@gmail.com>
    Date:   Wed May 18 02:51:53 2022 -0700
    
        Fix clang-format config
    
    commit fef4786fa8d5d607d501a6583a3059683dc14f19
    Author: Evan Straw <evan.straw99@gmail.com>
    Date:   Wed Mar 30 13:44:44 2022 -0700
    
        Try to do clangd config

--- modules/module-lang-c-c++.el
+++ modules/module-lang-c-c++.el
@@ -22,55 +22,66 @@
 (use-package clang-format
   :init
   (defun clang-format-auto ()
-	"Calls the appropriate clang-format function depending on
+    "Calls the appropriate clang-format function depending on
   whether or not a region is selected. If a region is selected,
   will apply clang-format-region to that region only; otherwise,
   will format the whole buffer."
-	(interactive)
-	(if (region-active-p)
-		(clang-format-region (region-beginning) (region-end))
-	  (clang-format-buffer)))
+    (interactive)
+    (if (region-active-p)
+	(clang-format-region (region-beginning) (region-end))
+      (clang-format-buffer)))
   :commands (clang-format-buffer
-			 clang-format-region
-			 clang-format-auto))
+	     clang-format-region
+	     clang-format-auto))
 
 ;; ---------------
-;; CCLS configuration (C/C++)
+;; Clangd configuration (C/C++)
 ;; ---------------
 
-(use-package ccls
+;;*  (use-package ccls
+;;*    :init
+;;*    ;; set up c styles
+;;*    (c-style-setup)
+;;*    ;; make it safe to set ccls-args in .dir-locals.el files
+;;*    ;;(put 'ccls-args 'safe-local-variable #'listp)
+;;*    :config
+;;*    (setq ccls-executable "/usr/local/bin/ccls")
+;;*    (setq company-minimum-prefix-length 1
+;;*  	company-idle-delay 0.0)
+;;*    (setq ccls-args '("--log-file=/tmp/ccls.log"
+;;*  		    "-v=1"))
+;;*    (setq ccls-initialization-options
+;;*  	'(:compilationDatabaseDirectory
+;;*  	  "build"
+;;*  	  :clang (:excludeArgs ["-fstrict-volatile-bitfields"
+;;*  				"-mlongcalls"
+;;*  				"-mfix-esp32-psram-cache-issue"]
+;;*  			       :extraArgs ["--gcc-toolchain=/usr"])))
+;;*    ;;		    "--init={\"compilationDatabaseDirectory\": \"build\"}"))
+;;*  					;
+;;*    ;;*  (setq ccls-initialization-options '(:clang (:extraArgs '("--gcc-toolchain" "/usr"))))
+;;*    :hook ((c-mode c++-mode objc-mode) .
+;;*           (lambda ()
+;;*  	   (require 'ccls)
+;;*  	   (lsp)))
+;;*    :bind (:map c-mode-map
+;;*  	      ("C-c C-f" . clang-format-auto)
+;;*  	      :map c++-mode-map
+;;*  	      ("C-c C-f" . clang-format-auto)))
+
+(use-package lsp-mode
+  :commands (lsp)
   :init
-  ;; set up c styles
-  (c-style-setup)
-  ;; make it safe to set ccls-args in .dir-locals.el files
-  ;;(put 'ccls-args 'safe-local-variable #'listp)
-  :config
-  (setq ccls-executable "/usr/local/bin/ccls")
-  (setq company-minimum-prefix-length 1
-		company-idle-delay 0.0)
-  (setq ccls-args '("--log-file=/tmp/ccls.log"
-					"-v=1"))
-  (setq ccls-initialization-options
-		'(:compilationDatabaseDirectory
-		  "build"
-		  :clang (:excludeArgs ["-fstrict-volatile-bitfields"
-								"-mlongcalls"
-								"-mfix-esp32-psram-cache-issue"]
-				  :extraArgs ["--gcc-toolchain=/usr"])))
-  ;;		    "--init={\"compilationDatabaseDirectory\": \"build\"}"))
-  ;
-;;*  (setq ccls-initialization-options '(:clang (:extraArgs '("--gcc-toolchain" "/usr"))))
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda ()
-		   (require 'ccls)
-		   (lsp)))
-  :bind (:map c-mode-map
-		 ("C-c C-f" . clang-format-auto)
-		 :map c++-mode-map
-		 ("C-c C-f" . clang-format-auto)))
+  (setq lsp-clients-clangd-executable "clangd")
+  :hook (((c-mode c++-mode objc-mode) . lsp)
+	 ((c-mode c++-mode objc-mode) . c-style-setup)
+	 (c-mode-common . (lambda () (setq-default tab-width 4)))))
 
-(add-hook 'c-mode-common-hook '(lambda ()
-				 (setq-default tab-width 4)))
+(use-package cc-mode
+  :bind (:map c-mode-map
+	      ("C-c C-f" . clang-format-auto)
+	 :map c++-mode-map
+	      ("C-c C-f" . clang-format-auto)))
 
 (use-package cmake-mode)
 
