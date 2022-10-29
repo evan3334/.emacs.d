a1575c35ec3a1e5d5f3ff20c7605f654e8e27447
Evan Straw <evan.straw99@gmail.com>
2022-10-29 11:26:14 -0700

    Add machine-select system allowing for per-machine config

--- init.el
+++ init.el
@@ -66,6 +66,35 @@ There are two things you can do about this warning:
 
 (global-set-key "\C-cd" 'open-dotfile)
 
+(let ((machine-select-file (expand-file-name "machine-select.el" modules-dir))
+      (machine-select-contents
+       ";;; machine-select -- Allows user to specify machine for config
+;;; Commentary:
+;;; This package defines one constant, MACHINE-SELECT-MACHINE, that allows the user to specify
+;;; which machine Emacs is currently running on, and allows the configuration to make different
+;;; choices based on the current machine.  For example, my desktop has a different screen size
+;;; than my laptop, so I would like a different font size on each machine.
+;;; Note that this file SHOULD NOT be backed up to version control or synchronized between
+;;; machines, unless you want them to have the same behavior; it will be created automatically
+;;; by the configuration if it does not exist.
+
+;;; Code:
+(defconst machine-select-machine 'default
+  \"This is the current machine that Emacs is running on.
+The value of this constant may change the behavior of the
+config.  Current valid values are 'laptop and 'desktop but others
+may be added later.\")
+
+(provide 'machine-select)
+;;; machine-select.el ends here"))
+  (when (not (file-exists-p machine-select-file))
+    (message "machine-select.el does not exist, creating...")
+    (with-temp-file machine-select-file
+      (insert machine-select-contents))
+    (message "machine-select.el created, but the value of machine-select-machine\
+ is not yet valid. You should visit the file and change it to a valid value."))
+  (require 'machine-select))
+
 (require 'module-appearance)
 (require 'module-git)
 (require 'module-lang-general)
