cab75e4d3f58d8de1ed4d26d0b53ebff91857183
Evan Straw <evan.straw99@gmail.com>
2023-04-05 20:17:00 -0700

    WIP changes to use hledger instead of ledger

--- modules/module-ledger.el
+++ modules/module-ledger.el
@@ -1,9 +1,12 @@
-(use-package ledger-mode
+(use-package hledger-mode
   ;;:custom (ledger-binary-path "hledger")
-  :custom (ledger-clear-whole-transactions t)
+  ;;:custom (ledger-clear-whole-transactions t)
   :mode "\\.journal\\'"
-  :hook '((ledger-mode . company-mode)
-	  (ledger-mode . emojify-mode)))
+  :hook '((hledger-mode . company-mode)
+	  (hledger-mode . emojify-mode)))
+
+(use-package flycheck-hledger
+  :after (flycheck hledger-mode))
 
 
 (provide 'module-ledger)
